9cbc0dc7a168d72c72c457ac1ff6a3bf9c8ba6be
PaikMyeongGyu <qoraudrb123@gmail.com>
2025-03-15 23:29:23 +0900

    [chore] - Refactoring 초기 세팅

---
+++ aop_problem/src/main/java/com/blog/aop_problem/AopProblemApplication.java
@@ -0,0 +1,13 @@
+package com.blog.aop_problem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopProblemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AopProblemApplication.class, args);
+    }
+
+}
---
+++ aop_problem/src/test/java/com/blog/aop_problem/AopProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.aop_problem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopProblemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
