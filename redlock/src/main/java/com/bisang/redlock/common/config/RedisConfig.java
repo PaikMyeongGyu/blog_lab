ab890f2cf445ea3cebddbfe803d0b58a8493bacf
PaikMyeongGyu <qoraudrb123@gmail.com>
2025-03-30 09:06:01 +0900

    [chore] : Redis 설정
    - Redis 설정과 관련된 직렬화 부분을 조금 다시 알아볼 필요가 있는 것 같다.

---
+++ redlock/src/main/java/com/bisang/redlock/common/config/RedisConfig.java
@@ -0,0 +1,46 @@
+package com.bisang.redlock.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    /**
+     * TODO : 일단 레디스에 관해서 Java에서 사용하는 방법에 대해서 더 익힐 필요가 있을 것 같다.
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+
+        // Redis 연결
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        // Key-Value 형태로 직렬화 수행
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+
+        // Value / HashValue 는 JSON 직렬화
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return redisTemplate;
+    }
+}
