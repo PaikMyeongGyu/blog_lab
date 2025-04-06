ceaea004f83f9bbd2e045d9fd13ae45721831ccc
PaikMyeongGyu <qoraudrb123@gmail.com>
2025-04-06 23:38:10 +0900

    [chore] : ?덈뵒??罹먯떛 ?곗뒿 ?꾨줈?앺듃 ?쒖옉
    - ?덈뵒??罹먯떛?먯꽌 硫붾え由ъ뼇怨?evict ?꾨왂? redis ?몄뒪?댁뒪瑜??꾩슱 ???ㅼ젙?????덈떎.

---
+++ redis-cache/src/main/java/com/cache/rediscache/config/RedisConfig.java
@@ -0,0 +1,43 @@
+package com.cache.rediscache.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+
+        // Redis 연결
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        // Key-Value 형태로 직렬화 수행
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+
+        // Value / HashValue 는 JSON 직렬화
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return redisTemplate;
+    }
+}
