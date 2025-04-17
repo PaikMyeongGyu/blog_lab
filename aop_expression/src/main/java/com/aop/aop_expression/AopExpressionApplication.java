a7f2169c69f9f4f035ed8fe7f6da8f4c8522069e
PaikMyeongGyu <qoraudrb123@gmail.com>
2025-04-17 14:46:00 +0900

    [chore] : aop 표현 관련 프로젝트 시작

---
+++ aop_expression/src/main/java/com/aop/aop_expression/AopExpressionApplication.java
@@ -0,0 +1,13 @@
+package com.aop.aop_expression;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopExpressionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AopExpressionApplication.class, args);
+    }
+
+}
---
+++ aop_expression/src/test/java/com/aop/aop_expression/AopExpressionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aop.aop_expression;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopExpressionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
